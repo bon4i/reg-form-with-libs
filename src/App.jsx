48dfc8603154d6986a39644381e039b2d7db758e
bon4i <bonchigam@gmail.com>
2024-04-11 23:18:01 +0300

    Add react hook form & yup

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,63 @@
-import { useRef, useState } from 'react';
+import React from "react";
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 import styles from './App.module.css';
-import React from 'react';
-import { EMAIL_REGEXP } from './utils';
-import { PASS_REGEXP } from './utils';
-import { emailErrors } from './utils';
-import { passwordErrors } from './utils';
-import { sendFormData } from './utils';
 import { InputField } from './components/InputField/InputField';
 
+const fieldsScheme = yup.object()
+	.shape({
+		email: yup
+		.string()
+			.matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+			'Неверная почта. Данные должны быть в формате @email.ru!')
+			.min(3, 'Неверная почта. Должно быть не меньше 3 символов!'),
+		password: yup
+			.string()
+			.matches(/(?=.*[0-9])(?=.*[a-z])/g,
+			'Пароль должен содержать хотя-бы одно число и хотя-бы одну латинскую букву в нижнем регистре')
+			.min(6, 'Слишком легкий пароль. Пароль должен быть не короче 6 символов!'),
+		confirmedPassword: yup
+			.string()
+			.oneOf([yup.ref('password'), null], 'Введенные пароли не совпадают'),
+	});
+
 export const App = () => {
-	const [formData, setFormData] = useState({
-		email: '',
-		password: '',
-		confirmedPassword: '',
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: {errors},
+	} = useForm({
+		defaultValues: {
+			email: '',
+			password: '',
+			confirmedPassword: '',
+		},
+		mode: 'onChange',
+		resolver: yupResolver(fieldsScheme),
 	});
 
-	const [errorData, setErrorData] = useState({
-		emailError: '',
-		passwordError: '',
-		confirmedPasswordError: '',
-	});
-
-	const [disabledSubmit, setDisabledSubmit] = useState(true)
-	const [sumbitOnFocus, setSumbitOnFocus] = useState(false);
-	const submitButtonRef = useRef(null);
-
-	const onEmailChange = ({target}) => {
-		setFormData(prevData => ({
-			...prevData,
-			email: target.value,
-		}));
-		if (!EMAIL_REGEXP.test(target.value)) {
-			setErrorData({...errorData, emailError: emailErrors.formatError});
-		} else if (target.value.length > 30) {
-			setErrorData({...errorData, emailError: emailErrors.maxLengthError});
-		} else if (target.value.length < 3) {
-			setErrorData({...errorData, emailError: emailErrors.minLengthError});
-		} else {
-			clearError();
-		}
+	const checkError = (errors) => {
+		let errorMessage = null;
+		switch (errorMessage) {
+			case errors.email:
+				errorMessage = errors.email?.message;
+			break;
+			case errors.password:
+				errorMessage = errors.password?.message;
+			break;
+			case errors.confirmedPassword:
+				errorMessage = errors.confirmedPassword?.message;
+			break;
+			default:
+			break;
+		};
 	}
 
-	const onPasswordChange = ({target}) => {
-		setFormData({
-			...formData,
-			password: target.value,
-		});
-		if (!PASS_REGEXP.test(target.value)) {
-			setErrorData({...errorData, passwordError: passwordErrors.formatError});
-		} else if (target.value.length < 6) {
-			setErrorData({...errorData, passwordError: passwordErrors.minLengthError});
-		} else {
-			clearError();
-		}
-	}
-
-	const onConfirmedPasswordChange = ({target}) => {
-		setFormData({
-			...formData,
-			confirmedPassword: target.value,
-		});
-		if (formData.password !== target.value) {
-			setErrorData({...errorData, confirmedPasswordError: passwordErrors.checkPasswordError});
-			setDisabledSubmit(true);
-		} else {
-			clearError();
-			checkFieldValidation();
-			setSumbitOnFocus(true);
-		}
-	}
-
-	const onSubmitRegistration = (event) => {
-		event.preventDefault();
-		sendFormData(formData);
-	}
-
-	const clearError = () => {
-		setErrorData({
-			emailError: '',
-			passwordError: '',
-			confirmedPasswordError: '',
-		});
-	}
-
-	const emailErrorHTML = errorData.emailError && <div className={styles.error}>{errorData.emailError}</div>;
-	const passwordErrorHTML = errorData.passwordError && <div className={styles.error}>{errorData.passwordError}</div>;
-    const confirmedPasswordErrorHTML = errorData.confirmedPasswordError && <div className={styles.error}>{errorData.confirmedPasswordError}</div>;
-
-	const checkFieldValidation = () => {
-		if (formData.email && formData.password && formData.confirmedPassword) {
-			setDisabledSubmit(false);
-		}
-	}
-
-	if (sumbitOnFocus === true) {
-		setTimeout(() => {
-			submitButtonRef.current.focus();
-			setSumbitOnFocus(false);
-		}, 1)
+	const submitForm = (formData) => {
+		console.log(formData);
+		reset();
 	}
 
 	return (
@@ -105,38 +65,32 @@ export const App = () => {
 			<div className={styles.header}>
 				<p className={styles.title}>Регистрация</p>
 			</div>
-			<form className={styles.form} onSubmit={onSubmitRegistration}>
-					<InputField
-						label={'Почта'}
-						type={'email'}
-						id={'email'}
-						value={formData.email}
-						name={'email'}
-						placeholder='Введите адрес ящика в формате @example.ru'
-						onChange={onEmailChange}
-					/>
-					<InputField
-						label={'Пароль'}
-						type={'password'}
-						id={'password'}
-						value={formData.password}
-						name={'password'}
-						placeholder='Введите пароль'
-						onChange={onPasswordChange}
-					/>
-					<InputField
-						label={'Повторите пароль'}
-						type={'password'}
-						id={'confirmedPassword'}
-						value={formData.confirmedPassword}
-						name={'confirmedPassword'}
-						placeholder='Повторите пароль'
-						onChange={onConfirmedPasswordChange}
-					/>
-					{emailErrorHTML || passwordErrorHTML || confirmedPasswordErrorHTML}
-					<button ref={submitButtonRef} type='submit' disabled={disabledSubmit}>
-						Зарегистроваться
-					</button>
+			<form className={styles.form} onSubmit={handleSubmit(submitForm)}>
+				<InputField
+					label={'Почта'}
+					type={'text'}
+					name={'email'}
+					placeholder='Введите адрес ящика в формате @example.ru'
+					{...register('email')}
+				/>
+				<InputField
+					label={'Пароль'}
+					type={'password'}
+					name={'password'}
+					placeholder='Введите пароль'
+					{...register('password')}
+				/>
+				<InputField
+					label={'Повторите пароль'}
+					type={'password'}
+					name={'confirmedPassword'}
+					placeholder='Повторите пароль'
+					{...register('confirmedPassword')}
+				/>
+				<div className={styles.error}>{checkError(errors)}</div>
+				<button type='submit'>
+					Зарегистроваться
+				</button>
 			</form>
 		</div>
 	);
--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,37 @@
-export const EMAIL_REGEXP = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-export const PASS_REGEXP = /(?=.*[0-9])(?=.*[a-z])/g;
-export const emailErrors = {
-    formatError: 'Неверная почта. Данные должны быть в формате @email.ru!',
-    maxLengthError: 'Неверная почта. Должно быть не больше 30 символов!',
-    minLengthError: 'Неверная почта. Должно быть не меньше 3 символов!',
-};
-export const passwordErrors = {
-    formatError:
-        'Пароль должен содержать хотя-бы одно число и хотя-бы одну латинскую букву в нижнем регистре',
-    minLengthError: 'Слишком легкий пароль. Пароль должен быть не короче 6 символов!',
-    checkPasswordError: 'Введенные пароли не совпадают!',
-};
+import * as yup from 'yup';
+
 export const sendFormData = (formData) => {
     console.log(formData);
+};
+
+export const emailChangeSchema = yup
+    .string()
+    .matches(
+        /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+        'Неверная почта. Данные должны быть в формате @email.ru!',
+    );
+
+export const emailBlurSchema = yup
+    .string()
+    .min(3, 'Неверная почта. Должно быть не меньше 3 символов!');
+
+export const passwordChangeSchema = yup
+    .string()
+    .matches(
+        /(?=.*[0-9])(?=.*[a-z])/g,
+        'Пароль должен содержать хотя-бы одно число и хотя-бы одну латинскую букву в нижнем регистре',
+    );
+
+export const passwordBlurSchema = yup
+    .string()
+    .min(6, 'Слишком легкий пароль. Пароль должен быть не короче 6 символов!');
+
+export const validateAndGetErrorMessage = (schema, value) => {
+    let errorMessage = null;
+    try {
+        schema.validateSync(value, { aboutEarly: false });
+    } catch ({ errors }) {
+        errorMessage = errors.join('\n');
+    }
+    return errorMessage;
 };
